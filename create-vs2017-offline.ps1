56c9d0b159332fb6e7f58194f9f7e7e10c0a7904
Bill Lewis <wrljet@gmail.com>
2021-09-18 17:44:30 -0400

    Correct timestamp in comment

--- create-vs2017-offline.ps1
+++ create-vs2017-offline.ps1
@@ -1,7 +1,7 @@
 # create-vs2017-offline.ps1 -- Part of Hercules-Helper
 #
 # SDL-Hercules-390 builder
-# Updated: 21 FEB 2021
+# Updated: 18 SEP 2021
 #
 # The most recent version of this project can be obtained with:
 #   git clone https://github.com/wrljet/hercules-helper.git
